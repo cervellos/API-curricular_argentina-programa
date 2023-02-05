305185c7d065597f740d78d9c142e16a1f3b3c05
Freddy Guerra <arcobaleno262@gmail.com>
2023-02-05 20:14:14 -0300

    montando tmeplate

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 console.log(document.querySelector("title").innerText);
+
+document.addEventListener("DOMContentLoaded", () => {
+  fetchData();
+});
+
+const fetchData = async () => {
+  try {
+    const res = await fetch("https://randomuser.me/api/");
+    const data = await res.json();
+    console.log(data);
+  } catch (error) {
+    console.log(`error en fechtDATA${error}`);
+  }
+};
