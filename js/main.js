21fb8ce9145aeecd7b7e081aaee01c1d61f83428
Freddy Guerra <arcobaleno262@gmail.com>
2023-02-05 20:14:49 -0300

    montando template

--- js/main.js
+++ js/main.js
@@ -9,6 +9,9 @@ const fetchData = async () => {
     const res = await fetch("https://randomuser.me/api/");
     const data = await res.json();
     console.log(data);
+    const curriculum = {
+      img: results[0].picture.large,
+    };
   } catch (error) {
     console.log(`error en fechtDATA${error}`);
   }
